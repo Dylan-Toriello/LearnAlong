32bca8b7113bc1c5f1318c2e3535ea7363bed524
Rishik15 <ry248@njit.edu>
2025-06-16 21:45:13 -0400

    Added basic structure to the project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold underline">
+        Hello...
+      </h1>
+      <button class="btn btn-primary">Button</button>
+    </>
+  )
+}
+
+export default App
